eb0d967a9ac9388ca986e86864af923e9d1fdc18
Karter13 <KarMiH1975@mail.ru>
2019-08-20 20:35:27 +0300

    создал стартовый шаблон для написания Дипломной работы

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// let button = document.querySelector("#button");
+// let modal = document.querySelector("#modal");
+
+// получение кнопки ЗАКРЫТЬ
+// let close = document.querySelector("#close");
+
+
+// button.addEventListener("click", function() {
+//     modal.classList.add("modal_activ");
+
+//     window.setTimeout(function() {
+//         modal.classList.remove("modal_activ");
+//     }, 5000);
+
+// });
+
+
+// function timer() {
+//   modal.classList.remove("modal_activ");
+// }
+
+//  убирает модальное окно при нажатии кнопки ЗАКРЫТЬ
+
+// close.addEventListener("click", function () {
+//   modal.classList.remove("modal_activ");
+// });
+
+
---
+++ src/js/modal.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    let button = $("#button");
+    let modal = $("#modal");
+    let close = $("#close");
+    let cardLink = $(".card__link");
+    let left = $(".modal-column_left");
+    let modalof = $(".modal-dialog");
+
+
+    /*Для кнопки Перезвони мне*/
+    button.on("click", function () {
+        left.addClass("modal_left");
+        modalof.css({ "max-width": "500px" });
+        modal.addClass("modal_activ");
+    });
+
+    /*Для блок price*/
+    cardLink.on("click", function (event) {
+        event.preventDefault();
+        modal.addClass("modal_activ");
+        let target = $(this).attr("data-target");
+        $(".modal-content").load(target + ".html");
+    });
+
+
+    close.on("click", function () {
+        modal.removeClass("modal_activ");
+        left.removeClass("modal_left");
+        modalof.css({ "max-width": "50%" });
+    });
+
+});
+
+
+
+
+
+
