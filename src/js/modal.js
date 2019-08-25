a67e816f9e8a9d8a4e7711327c9da0a6fa637da3
Karter13 <KarMiH1975@mail.ru>
2019-08-25 20:29:04 +0300

    сверстал модалку

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,36 +1,36 @@
 
-$(document).ready(function () {
-    let button = $("#button");
-    let modal = $("#modal");
-    let close = $("#close");
-    let cardLink = $(".card__link");
-    let left = $(".modal-column_left");
-    let modalof = $(".modal-dialog");
+// $(document).ready(function () {
+//     let button = $("#button");
+//     let modal = $("#modal");
+//     let close = $("#close");
+//     let cardLink = $(".card__link");
+//     let left = $(".modal-column_left");
+//     let modalof = $(".modal-dialog");
 
 
-    /*Для кнопки Перезвони мне*/
-    button.on("click", function () {
-        left.addClass("modal_left");
-        modalof.css({ "max-width": "500px" });
-        modal.addClass("modal_activ");
-    });
+//     /*Для кнопки Перезвони мне*/
+//     button.on("click", function () {
+//         left.addClass("modal_left");
+//         modalof.css({ "max-width": "500px" });
+//         modal.addClass("modal_activ");
+//     });
 
-    /*Для блок price*/
-    cardLink.on("click", function (event) {
-        event.preventDefault();
-        modal.addClass("modal_activ");
-        let target = $(this).attr("data-target");
-        $(".modal-content").load(target + ".html");
-    });
+//     /*Для блок price*/
+//     cardLink.on("click", function (event) {
+//         event.preventDefault();
+//         modal.addClass("modal_activ");
+//         let target = $(this).attr("data-target");
+//         $(".modal-content").load(target + ".html");
+//     });
 
 
-    close.on("click", function () {
-        modal.removeClass("modal_activ");
-        left.removeClass("modal_left");
-        modalof.css({ "max-width": "50%" });
-    });
+//     close.on("click", function () {
+//         modal.removeClass("modal_activ");
+//         left.removeClass("modal_left");
+//         modalof.css({ "max-width": "50%" });
+//     });
 
-});
+// });
 
 
 
